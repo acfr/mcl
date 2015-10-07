b2e98acb1424bbbe7733cb933f129e41da1aa784
Asher Bender <a.bender@acfr.usyd.edu.au>
2015-10-07 17:08:18 +1100

    logging: ReadFile() unit-tests.

--- mcl/logging/test/test_file.py
+++ mcl/logging/test/test_file.py
@@ -4,14 +4,17 @@ import unittest
 
 import mcl.message.messages
 from mcl.logging.file import WriteFile
+from mcl.logging.file import ReadFile
 from mcl.network.abstract import Connection as AbstractConnection
 
 _DIRNAME = os.path.dirname(__file__)
-LOG_PATH = os.path.join(_DIRNAME, 'tmp')
+TMP_PATH = os.path.join(_DIRNAME, 'tmp')
+LOG_PATH = os.path.join(_DIRNAME, 'dataset')
+SPT_PATH = os.path.join(_DIRNAME, 'dataset_split')
 
 
-class UnitTestMessage(mcl.message.messages.Message):
-    mandatory = ('A', 'B',)
+class UnitTestMessageA(mcl.message.messages.Message):
+    mandatory = ('data',)
     connection = AbstractConnection()
 
 
@@ -24,18 +27,18 @@ class WriteFileTests(unittest.TestCase):
     def setUp(self):
         """Create logging path if it does not exist."""
 
-        if not os.path.exists(LOG_PATH):
-            os.makedirs(LOG_PATH)
+        if not os.path.exists(TMP_PATH):
+            os.makedirs(TMP_PATH)
 
-        with open(os.path.join(LOG_PATH, 'README'), 'w') as f:
+        with open(os.path.join(TMP_PATH, 'README'), 'w') as f:
             f.write('This directory was created automatically\n')
             f.write('for unit-testing & can be safely deleted.\n')
 
     def tearDown(self):
         """Delete files created for test logging."""
 
-        if os.path.exists(LOG_PATH):
-            shutil.rmtree(LOG_PATH)
+        if os.path.exists(TMP_PATH):
+            shutil.rmtree(TMP_PATH)
 
     def delete_if_exists(self, fname):
         """Delete file if it exists."""
@@ -46,28 +49,28 @@ class WriteFileTests(unittest.TestCase):
     def test_bad_init(self):
         """Test WriteFile() catches bad initialisation."""
 
-        prefix = os.path.join(LOG_PATH, 'unittest')
+        prefix = os.path.join(TMP_PATH, 'unittest')
 
         # Ensure max_entries is specified properly.
         with self.assertRaises(TypeError):
-            WriteFile(prefix, UnitTestMessage, max_entries='a')
+            WriteFile(prefix, UnitTestMessageA, max_entries='a')
 
         # Ensure max_time is specified properly.
         with self.assertRaises(TypeError):
-            WriteFile(prefix, UnitTestMessage, max_time='a')
+            WriteFile(prefix, UnitTestMessageA, max_time='a')
 
     def test_initialisation(self):
         """Test WriteFile() initialisation with no splitting."""
 
         # Create prefix for data log.
-        prefix = os.path.join(LOG_PATH, 'unittest')
+        prefix = os.path.join(TMP_PATH, 'unittest')
         tmp = prefix + '.tmp'
 
         # Delete log if it already exists (it shouldn't).
         self.delete_if_exists(tmp)
 
         # Create logging object.
-        wf = WriteFile(prefix, UnitTestMessage)
+        wf = WriteFile(prefix, UnitTestMessageA)
         self.assertEqual(tmp, wf._WriteFile__get_filename())
 
         # Log file does not exist until data has been written.
@@ -78,7 +81,7 @@ class WriteFileTests(unittest.TestCase):
         """Test WriteFile() initialisation with splitting."""
 
         # Create prefix for data log.
-        prefix = os.path.join(LOG_PATH, 'unittest')
+        prefix = os.path.join(TMP_PATH, 'unittest')
         tmp = prefix + '_000.tmp'
 
         # Delete split log if it already exists (it shouldn't).
@@ -89,7 +92,7 @@ class WriteFileTests(unittest.TestCase):
         max_time = 60
 
         # Create split logging object.
-        wf = WriteFile(prefix, UnitTestMessage,
+        wf = WriteFile(prefix, UnitTestMessageA,
                        max_entries=max_entries,
                        max_time=max_time)
         self.assertEqual(tmp, wf._WriteFile__get_filename())
@@ -104,7 +107,7 @@ class WriteFileTests(unittest.TestCase):
         """Test WriteFile() write single file."""
 
         # Create file names.
-        prefix = os.path.join(LOG_PATH, 'unittest')
+        prefix = os.path.join(TMP_PATH, 'unittest')
         tmp = prefix + '.tmp'
         log = prefix + '.log'
 
@@ -113,7 +116,7 @@ class WriteFileTests(unittest.TestCase):
         self.delete_if_exists(log)
 
         # Create data for testing.
-        test_message = UnitTestMessage(A=1, B=2)
+        test_message = UnitTestMessageA(data=None)
 
         # Package up message object.
         message = {'time_received': None,
@@ -121,7 +124,7 @@ class WriteFileTests(unittest.TestCase):
                    'payload': test_message.encode()}
 
         # Create logging object.
-        wf = WriteFile(prefix, UnitTestMessage)
+        wf = WriteFile(prefix, UnitTestMessageA)
         self.assertFalse(os.path.exists(tmp))
 
         # Log data and ensure file exists.
@@ -151,7 +154,7 @@ class WriteFileTests(unittest.TestCase):
         """Test WriteFile() write split files."""
 
         # Create file names.
-        prefix = os.path.join(LOG_PATH, 'unittest')
+        prefix = os.path.join(TMP_PATH, 'unittest')
         tmp0 = prefix + '_000.tmp'
         tmp1 = prefix + '_001.tmp'
         log0 = prefix + '_000.log'
@@ -162,7 +165,7 @@ class WriteFileTests(unittest.TestCase):
             self.delete_if_exists(fname)
 
         # Create data for testing.
-        test_message = UnitTestMessage(A=1, B=2)
+        test_message = UnitTestMessageA(data=None)
 
         # Package up message object.
         message = {'time_received': None,
@@ -170,7 +173,7 @@ class WriteFileTests(unittest.TestCase):
                    'payload': test_message.encode()}
 
         # Create logging object.
-        wf = WriteFile(prefix, UnitTestMessage, max_entries=2)
+        wf = WriteFile(prefix, UnitTestMessageA, max_entries=2)
         wf.write(message)
         wf.write(message)
         self.assertTrue(os.path.exists(tmp0))
@@ -199,25 +202,31 @@ class WriteFileTests(unittest.TestCase):
             self.delete_if_exists(fname)
 
 
------------------------------------------------------------------------------
-                                 ReadFile()
------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#                                  ReadFile()
+# -----------------------------------------------------------------------------
 
 class ReadFileTests(unittest.TestCase):
 
     def test_initialisation(self):
         """Test ReadFile() initialisation."""
 
-        min_time = 1.0
-        max_time = 10.0
-
-        # Path to valid log file.
-        fname = os.path.join(_DIRNAME, './dataset/GnssMessage.log')
-        rf = ReadFile(fname, min_time=min_time, max_time=max_time)
+        # Create file reader object.
+        fname = os.path.join(LOG_PATH, 'UnitTestMessageA.log')
+        rf = ReadFile(fname)
 
         # Test access to properties.
-        self.assertEqual(min_time, rf.min_time)
-        self.assertEqual(max_time, rf.max_time)
+        self.assertEqual(rf.min_time, None)
+        self.assertEqual(rf.max_time, None)
+
+        # Ensure object can parse the header block.
+        self.assertNotEqual(rf.header, None)
+        self.assertEqual(rf.header['message'], UnitTestMessageA)
+
+    def test_bad_init(self):
+        """Test ReadFile() catches bad initialisation."""
+
+        fname = os.path.join(LOG_PATH, 'UnitTestMessageA.log')
 
         # Ensure failure on files that do not exit.
         with self.assertRaises(IOError):
@@ -238,19 +247,16 @@ class ReadFileTests(unittest.TestCase):
     def test_read_single(self):
         """Test ReadFile() read single file."""
 
-        # Path to valid log file.
-        fname = os.path.join(_DIRNAME, './dataset/GnssMessage.log')
+        # Create file reader object.
+        fname = os.path.join(LOG_PATH, 'UnitTestMessageA.log')
         rf = ReadFile(fname)
 
-        # Ensure object can parse the header block.
-        self.assertNotEqual(rf.header, None)
-        self.assertEqual(rf.header['message'], GnssMessage)
-
         # Ensure items in file can be read correctly.
-        for i in range(1, 11):
+        for i in range(0, 10):
             self.assertTrue(rf.is_data_pending())
             message = rf.read()
             self.assertEqual(int(10 * message['elapsed_time']), i)
+            self.assertEqual(int(10 * message['message']['timestamp']), i)
 
         # Ensure None is returned when all data has been read.
         self.assertFalse(rf.is_data_pending())
@@ -260,14 +266,23 @@ class ReadFileTests(unittest.TestCase):
     def test_read_time(self):
         """Test ReadFile() read min/max time."""
 
-        # Path to valid log file.
-        fname = os.path.join(_DIRNAME, './dataset/GnssMessage.log')
+        # Define time interval of interest.
+        min_time = 0.35
+        max_time = 0.85
+
+        # Create file reader object.
+        fname = os.path.join(LOG_PATH, 'UnitTestMessageA.log')
         rf = ReadFile(fname, min_time=0.35, max_time=0.85)
+
+        # Ensure time ranges were set.
+        self.assertEqual(rf.min_time, min_time)
+        self.assertEqual(rf.max_time, max_time)
 
         # Ensure object can filter items by time .
         for i in range(4, 9):
             message = rf.read()
             self.assertEqual(int(10 * message['elapsed_time']), i)
+            self.assertEqual(int(10 * message['message']['timestamp']), i)
 
         # Ensure None is returned when all data has been read.
         message = rf.read()
@@ -276,59 +291,74 @@ class ReadFileTests(unittest.TestCase):
     def test_reset(self):
         """Test ReadFile() reset."""
 
-        # Path to valid log file.
-        fname = os.path.join(_DIRNAME, './dataset/GnssMessage.log')
+        # Create file reader object.
+        fname = os.path.join(LOG_PATH, 'UnitTestMessageA.log')
         rf = ReadFile(fname)
 
         # Read first five items.
-        for i in range(1, 6):
+        for i in range(0, 6):
             message = rf.read()
             self.assertEqual(int(10 * message['elapsed_time']), i)
+            self.assertEqual(int(10 * message['message']['timestamp']), i)
 
         # Reset object.
         rf.reset()
 
         # Re-read all data.
-        for i in range(1, 11):
+        for i in range(0, 10):
             message = rf.read()
             self.assertEqual(int(10 * message['elapsed_time']), i)
+            self.assertEqual(int(10 * message['message']['timestamp']), i)
 
     def test_read_split(self):
         """Test ReadFile() read split files."""
 
-        # Path to valid log file.
-        fname = os.path.join(_DIRNAME, './dataset_split/GnssMessage')
+        # Prefix of split log files.
+        fname = os.path.join(SPT_PATH, 'UnitTestMessageA_split_time')
         rf = ReadFile(fname)
 
         # Ensure object can parse the header block.
         self.assertNotEqual(rf.header, None)
-        self.assertEqual(rf.header['message'], GnssMessage)
+        self.assertEqual(rf.header['message'], UnitTestMessageA)
 
-        # Ensure items in file can be read correctly.
-        for i in range(1, 11):
+        # Ensure items in split log-files can be read correctly.
+        for i in range(0, 15):
             self.assertTrue(rf.is_data_pending())
             message = rf.read()
             self.assertEqual(int(10 * message['elapsed_time']), i)
+            self.assertEqual(int(10 * message['message']['timestamp']), i)
 
         # Ensure None is returned when all data has been read.
+        self.assertFalse(rf.is_data_pending())
         message = rf.read()
         self.assertEqual(message, None)
 
     def test_read_partial(self):
         """Test ReadFile() read one split file."""
 
-        # Path to valid log file.
-        fname = os.path.join(_DIRNAME, './dataset_split/GnssMessage_002.log')
+        # Path of a single split log file.
+        fname = os.path.join(SPT_PATH, 'UnitTestMessageA_split_time_001.log')
         rf = ReadFile(fname)
 
         # There is no header block to read. Ensure object returns None.
         self.assertEqual(rf.header, None)
 
-        # Ensure items in file can be read correctly.
-        for i in range(7, 11):
+        # Ensure items in split log-files can be read correctly.
+        for i in range(5, 10):
+            self.assertTrue(rf.is_data_pending())
             message = rf.read()
             self.assertEqual(int(10 * message['elapsed_time']), i)
+            self.assertEqual(int(10 * message['message']['timestamp']), i)
 
+        # Ensure None is returned when all data has been read.
+        self.assertFalse(rf.is_data_pending())
+        message = rf.read()
+        self.assertEqual(message, None)
+
+
+# -----------------------------------------------------------------------------
+#                             ReadDirectoryTests()
+# -----------------------------------------------------------------------------
 
 class ReadDirectoryTests(unittest.TestCase):
 
